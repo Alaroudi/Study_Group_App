1142db80dfacbae4d56f28836f6eeab867f733df
alaroudi <60710590+alaroudi@users.noreply.github.com>
2020-04-19 21:45:25 -0500

    Add files via upload

---
+++ Project/controller/FindStudyGroupsController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author alaro
+ */
+public class FindStudyGroupsController implements Initializable {
+
+    @FXML
+    private SplitPane FindStudyGroup;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
