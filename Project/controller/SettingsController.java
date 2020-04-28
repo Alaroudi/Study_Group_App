79cea06016cfb8c70fa18870e5a107f9863cab1b
alaroudi <60710590+alaroudi@users.noreply.github.com>
2020-04-28 00:11:26 -0500

    Update SettingsController.java

--- Project/controller/SettingsController.java
+++ Project/controller/SettingsController.java
@@ -1,95 +1,97 @@
 package Project.controller;
 
 import Project.model.UserInfo;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
 import javafx.event.ActionEvent;
-
 import java.io.*;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import java.util.Scanner;
+import javafx.scene.control.Label;
 
-/**
- * SettingsController class controls the Settings.fxml scene
- *
- * @autor Alana Quinones Garcia - ded687
- */
 public class SettingsController extends MainSceneController implements Initializable {
     @FXML
     private JFXTextField usernameField;
+
     @FXML
-    private JFXTextField passwordField;
+    private JFXPasswordField passwordField;
+
     @FXML
     private JFXTextField lastNameField;
+
     @FXML
     private JFXTextField firstNameField;
 
-    /**
-     * initialize(URL url, ResourceBundle rb) initializes the Settings.fxml scene
-     *
-     * @param url
-     * @param rb
-     */
+    @FXML
+    private JFXDatePicker birthdateField;
+    @FXML
+    private Label changesConfirm;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        firstNameField.setPromptText("First Name");
-        lastNameField.setPromptText("Last Name");
-        usernameField.setPromptText("Username");
-        passwordField.setPromptText("Password");
+      
     }
 
-    /**
-     * saveSettings(ActionEvent event) changes a user's settings when they click on 'save changes' button
-     *
-     * @param event
-     */
     @FXML
-    public void saveSettings(ActionEvent event) {
-        // handle empty input
-        String newFirstName = firstNameField.getText();
-        if (newFirstName.contentEquals("")) {
+    void saveSettings(ActionEvent event) {
+       
+        Scanner scan;
+        String newFirstName = firstNameField.getText().trim();
+        if(newFirstName.contentEquals("")) {
             newFirstName = user.getFirstname();
         }
-        String newLastName = lastNameField.getText();
-        if (newLastName.contentEquals("")) {
+        String newLastName = lastNameField.getText().trim();
+        if(newLastName.contentEquals("")) {
             newLastName = user.getLasttname();
         }
-        String newUsername = usernameField.getText();
-        if (newUsername.contentEquals("")) {
+
+        String newBirthDate;
+        if(birthdateField.getValue() != null) {
+            LocalDate date = birthdateField.getValue();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+            newBirthDate = formatter.format(date);
+        } 
+        else{
+            
+             newBirthDate = user.getBirthDate();
+        }
+        String newUsername = usernameField.getText().trim();
+        if(newUsername.contentEquals("")) {
             newUsername = user.getUsername();
         }
         String newPassword = passwordField.getText();
-        if (newPassword.contentEquals("")) {
+        if(newPassword.contentEquals("")) {
             newPassword = user.getPassword();
         }
+        
 
-        // create user with changed info
-        UserInfo changedUser = new UserInfo();
-        changedUser.setUserInfo(newFirstName, newLastName, user.getBirthDate(), newUsername, newPassword);
-        try {
-            // read from old user info file
-            File oldFile = new File("Project/model/Users.txt");
-            Scanner scan;
+        try{
+            File oldFile = new File("C:\\Users\\alaro\\Documents\\NetBeansProjects\\Project\\src\\Project\\model\\Users.txt");
             scan = new Scanner(oldFile);
             scan.useDelimiter("[,\n]");
 
-            // create tmp file
-            File tmpFile = new File("Project/model/tmpUsers.txt");
-            FileWriter fileWriter = new FileWriter("Project/model/tmpUsers.txt", false);
+            File tmpFile = new File("C:\\Users\\alaro\\Documents\\NetBeansProjects\\Project\\src\\Project\\model\\tmpUsers.txt");
+
+            FileWriter fileWriter = new FileWriter("C:\\Users\\alaro\\Documents\\NetBeansProjects\\Project\\src\\Project\\model\\tmpUsers.txt",false);
             BufferedWriter print = new BufferedWriter(fileWriter);
 
-            // read from old file into tmp file
             UserInfo tmpUser = new UserInfo();
-            while (scan.hasNext()) {
+            while(scan.hasNext())
+            {
                 tmpUser.setUserInfo(scan.next(), scan.next(), scan.next(), scan.next(), scan.next());
-                if (tmpUser.getUsername().equals(user.getUsername()) && tmpUser.getPassword().equals(user.getPassword())) {
-                    print.write(newFirstName + "," + newLastName + ","
-                            + user.getBirthDate() + "," + newUsername + ","
-                            + newPassword + "\n");
-                } else {
+                if (tmpUser.getUsername().equals(user.getUsername()) && tmpUser.getPassword().equals(user.getPassword()))
+                {
+                    print.write(newFirstName+ "," + newLastName + ","
+                            + newBirthDate + "," +  newUsername + ","
+                            + newPassword +"\n");
+                }
+                else {
                     print.write(tmpUser.getFirstname() + "," + tmpUser.getLasttname() + ","
                             + tmpUser.getBirthDate() + "," + tmpUser.getUsername() + ","
                             + tmpUser.getPassword() + "\n");
@@ -102,19 +104,22 @@ public class SettingsController extends MainSceneController implements Initializ
             print.flush();
             print.close();
             fileWriter.close();
-
-            boolean isDeleted = oldFile.delete();
-            File newFile = new File("Project/model/Users.txt");
-            boolean isRenamed = tmpFile.renameTo(newFile);
+            
+            oldFile.delete();
+            File newFile = new File("C:\\Users\\alaro\\Documents\\NetBeansProjects\\Project\\src\\Project\\model\\Users.txt");
+            tmpFile.renameTo(newFile);
 
             firstNameField.clear();
             lastNameField.clear();
             usernameField.clear();
             passwordField.clear();
-        } catch (FileNotFoundException e) {
+            changesConfirm.setText("Chaneges has been saved!");
+        }catch (FileNotFoundException e){
             System.out.println(e);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
 }
