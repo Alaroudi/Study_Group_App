58b42181954b0021b62445ab6865c476d67515f4
alaroudi <60710590+alaroudi@users.noreply.github.com>
2020-04-28 00:12:16 -0500

    Update CreateGroupController.java

--- Project/controller/CreateGroupController.java
+++ Project/controller/CreateGroupController.java
@@ -9,21 +9,32 @@ import java.io.IOException;
 import java.util.Scanner;
 
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTimePicker;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.text.TextAlignment;
 
 /**
  * CreateCroupController class controls the CreateGroupView.fxml scene
  *
  * @author Kassie Garcia
  */
-public class CreateGroupController {
+public class CreateGroupController implements Initializable {
     @FXML
     private JFXTextField dayTF;
     @FXML
-    private JFXTextField timeTF;
+    private JFXTimePicker timeTF;
     @FXML
     private JFXTextField locationTF;
     @FXML
@@ -32,10 +43,20 @@ public class CreateGroupController {
     private JFXTextField courseTypeTF;
     @FXML
     private Label errorLabel;
+    @FXML
+    private Spinner<?> seats;
 
-    private String studyGroupAdd, meetDay, meetTime, meetLocation, meetContact, meetCourseInfo;
+    private String studyGroupAdd, meetDay, meetTime, meetLocation, meetContact, CourseInfo, availableSeats;
 
     private Scanner scn;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        
+        SpinnerValueFactory quantityValue = new SpinnerValueFactory.IntegerSpinnerValueFactory(2,100,10);
+        seats.setValueFactory(quantityValue);
+    }
 
     /**
      * handleCreateGroup(ActionEvent event) creates a study group when user clicks on 'Create Group' button
@@ -47,124 +68,92 @@ public class CreateGroupController {
     private void handleCreateGroup(ActionEvent event) throws IOException { //when the create group button is clicked
     	//get rid of error label message if user tries again
         errorLabel.setText("");
+        errorLabel.setTextAlignment(TextAlignment.CENTER);
+        //get the text from all the text field
+        CourseInfo = courseTypeTF.getText().toUpperCase().trim();
+        //get the meet days and remove all withe spaces
+        meetDay = dayTF.getText().toUpperCase().replaceAll("\\s", "");
+        if (timeTF.getValue() != null){
+            meetTime = timeTF.getValue().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)).toString().replaceAll("\\s", "");
+        }   
+        meetLocation = locationTF.getText().toUpperCase().trim();
+        meetContact = contactTF.getText().toUpperCase().trim(); 
+        availableSeats = seats.getValue().toString();
         
-        //get the text from all the text fields
-        meetDay = dayTF.getText().toUpperCase();
-        meetTime = timeTF.getText().toUpperCase();
-        meetLocation = locationTF.getText().toUpperCase();
-        meetContact = contactTF.getText().toUpperCase();
-        meetCourseInfo = courseTypeTF.getText().toUpperCase();
-
+        //if the user input for course information is invalid format
+        if (!(CourseInfo.matches("^(CS||MAT) \\d{4} \\d{3}"))) {
+            //print error message to screen
+            errorLabel.setText("Invalid Course Information Format. Try again!");
+           
+            //exit the method
+            return;
+        }
+        
         //if user input of the meeting day is not  M T W R F S U or a combo of those letters
-        if (meetDay.contains("A") || meetDay.contains("B") || meetDay.contains("C") || meetDay.contains("D")
-                || meetDay.contains("E") || meetDay.contains("G") || meetDay.contains("H") || meetDay.contains("I")
-                || meetDay.contains("J") || meetDay.contains("K") || meetDay.contains("L") || meetDay.contains("N")
-                || meetDay.contains("O") || meetDay.contains("P") || meetDay.contains("Q") || meetDay.contains("V")
-                || meetDay.contains("X") || meetDay.contains("Y") || meetDay.contains("Z") || meetDay.length() > 7) {
+        if (!meetDay.matches("[MTWRFSU]+")|| meetDay.length() > 7) {
 
             //print error message to screen
-            errorLabel.setText("Invalid Day Format. Try again");
-            //clear the textfield
-            dayTF.clear();
+            errorLabel.setText("Invalid Day Format. Try again1");
+            
             //exit the method
             return;
 
         }
         //if we don't get a meeting time in ##:##PM/AM format
-        if (!(meetTime.matches("^\\d{2}:\\d{2}PM") || meetTime.matches("^\\d{2}:\\d{2}AM") || meetTime.matches("^\\d{1}:\\d{2}PM")
-                || meetTime.matches("^\\d{1}:\\d{2}AM"))) {
+        if (!(meetTime.matches("^\\d{1,2}:\\d{2}(AM||PM)"))) {
 
             //print error message to screen
-            errorLabel.setText("Invalid Time Format. Try again");
-            //clear the textfield
-            timeTF.clear();
+            errorLabel.setText("Invalid Time Format. Try again!");
+            
             //exit the method
             return;
         }
         //if the user input for meeting location isnt an UTSA Building acronym
-        if (!(meetLocation.equals("SU") || meetLocation.equals("MS") || meetLocation.equals("ART") || meetLocation.equals("JPL")
-                || meetLocation.equals("FLN") || meetLocation.equals("EB") || meetLocation.equals("AET") || meetLocation.equals("BSB")
-                || meetLocation.equals("BSE") || meetLocation.equals("HSU") || meetLocation.equals("IAB") || meetLocation.equals("CC")
-                || meetLocation.equals("RWC") || meetLocation.equals("MH") || meetLocation.equals("BB") || meetLocation.equals("NPB")
-                || meetLocation.equals("GSR") || meetLocation.equals("RRC") || meetLocation.equals("BQS") || meetLocation.equals("LV")
-                || meetLocation.equals("AH") || meetLocation.equals("CV") || meetLocation.equals("UO") || meetLocation.equals("CH"))) {
+        if (meetLocation.equals("")) {
 
             //print error message to screen
-            errorLabel.setText("Invalid Location Format. Try again");
-            //clear the textfield
-            locationTF.clear();
+            errorLabel.setText("Invalid Location Format. Try again!");
+            
             //exit the method
             return;
         }
         //the input here is less restricted, basically anything with no spaces and an @ or numbers
         // as contact info can be an email or a phone number
-        if (meetContact.contains(" ") || !(meetContact.contains("@") || meetContact.matches(".*\\d.*"))) {
+        if (meetContact.equals("")) {
             //print error message to screen
-            errorLabel.setText("Invalid Contact Information Format. Try again");
-            //clear the textfield
+            errorLabel.setText("Invalid Contact Information Format. Try again!");
+            
+            //exit the method
+            return;
+        }
+        
+        
+        if (verifyCourse(CourseInfo)){
+            studyGroupAdd = " " + meetDay + " " + meetTime + " " + meetLocation + " " + meetContact+ " " + availableSeats;       
+            replaceSelected(CourseInfo, studyGroupAdd);
+            errorLabel.setText("Sucessfully added.");
+            dayTF.clear();
+            timeTF.setValue(LocalTime.now());
+            locationTF.clear();
             contactTF.clear();
-            //exit the method
-            return;
-        }
-        //if the user input for course information is invalid format
-        if (!(meetCourseInfo.matches("^CS \\d{4} \\d{3}") || meetCourseInfo.matches("^MAT \\d{4} \\d{3}") || meetCourseInfo.matches("^CS \\d{4} \\d{1}A\\d{1}")
-                || meetCourseInfo.matches("^CS \\d{4} \\d{1}B\\d{1}") || meetCourseInfo.matches("^CS \\d{4} \\d{1}C\\d{1}")
-                || meetCourseInfo.matches("^CS \\d{4} \\d{1}D\\d{1}"))) {
-            //print error message to screen
-            errorLabel.setText("Invalid Course Information Format. Try again");
-            //clear the textfield
             courseTypeTF.clear();
-            //exit the method
-            return;
+            
         }
-
-        //scan in the courseinfo file to read it
-        try {
-            scn = new Scanner(new File("courseinfo.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
-        //these lines will skip the beginning 2 lines that start with #
-        scn.nextLine();
-        scn.nextLine();
-
-        //while there's a line to scan within the file
-        while (scn.hasNextLine()) {
-            //add the study group info into the file
-            //if the course is found and it does not have a study group
-            if (meetCourseInfo.equals(scn.nextLine())) {
-            	//create the line that will be added to courseinfo.txt
-                studyGroupAdd = " " + meetDay + " " + meetTime + " " + meetLocation + " " + meetContact;
-                
-                //replace the previous line with our new line with all the study group info
-                replaceSelected(studyGroupAdd, meetCourseInfo);
-                errorLabel.setText("Sucessfully added.");
-                break;
-            }
-
-        }
-        //if the course already had a study group
-        if (!errorLabel.getText().equals("Sucessfully added.")) {
+        else{
+            
             errorLabel.setText("Course already has a study group.");
+            
+            
         }
-        //close the scanner
-        scn.close();
 
-        //clear all the text fields
-        dayTF.clear();
-        timeTF.clear();
-        locationTF.clear();
-        contactTF.clear();
-        courseTypeTF.clear();
+        
     }
 
     //a method that adds the study group information to the txt file
-    public static void replaceSelected(String replaceWith, String type) {
+    private void replaceSelected(String type,String replaceWith) {
         try {
             // input the file content to the StringBuffer "inputBuffer"
-            BufferedReader file = new BufferedReader(new FileReader("courseinfo.txt"));
+            BufferedReader file = new BufferedReader(new FileReader("C:\\Users\\alaro\\Documents\\NetBeansProjects\\Project\\src\\Project\\model\\courseinfo.txt"));
             StringBuffer inputBuffer = new StringBuffer();
             String line;
             //while there's a line to read, add to inputBuffer 
@@ -181,13 +170,44 @@ public class CreateGroupController {
             inputStr = inputStr.replaceAll(type, type + replaceWith);
 
             // write the new string with the replaced line OVER the same file
-            FileOutputStream fileOut = new FileOutputStream("courseinfo.txt");
+            FileOutputStream fileOut = new FileOutputStream("C:\\Users\\alaro\\Documents\\NetBeansProjects\\Project\\src\\Project\\model\\courseinfo.txt");
             fileOut.write(inputStr.getBytes());
             fileOut.close();
 
         } catch (Exception e) {
             System.out.println("Problem reading file.");
         }
+    }
+    
+    private boolean verifyCourse(String course){
+        
+         //scan in the courseinfo file to read it
+        try {
+            scn = new Scanner(new File("C:\\Users\\alaro\\Documents\\NetBeansProjects\\Project\\src\\Project\\model\\courseinfo.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        
+
+         //these lines will skip the beginning 2 lines that start with #
+        scn.nextLine();
+        scn.nextLine();
+        String fileCourse;
+        //while there's a line to scan within the file
+        while (scn.hasNextLine()) {
+            //add the study group info into the file
+            //if the course is found and it does not have a study group
+            fileCourse = scn.nextLine().trim();
+            if (course.equals(fileCourse)) {
+                
+            	return true;
+            }
+
+        }
+        scn.close();
+        return false;
+     
     }
 }
 	
